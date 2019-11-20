4f016b4ac48b03381c2ce9063683ad5d435a5c31
Jason J Cruz <jaycru06@yahoo.com>
2019-11-20 20:27:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,6 @@ end
 def take_a_number(katz_deli, name) 
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
-  # katz_deli.each_with_index do |name, position| puts "Welcome, #{name}. You are number #{position+1} in line."
-  # end 
 end
 
 
