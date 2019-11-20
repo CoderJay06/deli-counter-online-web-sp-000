795e069eb8a79507aab995c3972183a9253e75a4
Jason J Cruz <jaycru06@yahoo.com>
2019-11-20 20:23:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+# katz_deli = []
+# other_deli = ["Logan", "Avi", "Spencer"]
+# another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(katz_deli) 
+  current_line = "The line is currently:"
+  if katz_deli.count > 1 
+     katz_deli.each_with_index do |customer, position| current_line << " #{position+1}. #{customer}"
+     end
+  else 
+    current_line = "The line is currently empty."
+  end
+  puts current_line
+end 
+
+def take_a_number(katz_deli, name) 
+  katz_deli.push(name)
+  while katz_deli.count > 0
+    katz_deli.each_with_index do |name, position| puts "Welcome, #{name}. You are number #{position+1} in line."
+    break
+    end
+  end 
+end
+
+
+
+
+
+
+
+
